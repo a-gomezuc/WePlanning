8dd8dde1337b179d114a62dee79ed76afd8cb91d
Jorge Sánchez <jsm0594@gmail.com>
2017-02-28 10:09:59 +0100

    Sclipse

---
+++ WePlanning/src/main/java/com/example/WePlanningApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//Jamon
+@SpringBootApplication
+public class WePlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WePlanningApplication.class, args);
+	}
+}
