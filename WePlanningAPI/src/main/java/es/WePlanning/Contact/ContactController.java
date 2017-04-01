250593ae8c42678527876dd1b379e560ad3bb08f
rgolderos <r.golderos@alumnos.urjc.es>
2017-04-01 13:05:33 +0100

    error fixed

--- WePlanningAPI/src/main/java/es/WePlanning/Contact/ContactController.java
+++ WePlanningAPI/src/main/java/es/WePlanning/Contact/ContactController.java
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
 
 import es.WePlanning.Contact.Contact;
-
+@RestController
 public class ContactController {
 	
 	@Autowired
