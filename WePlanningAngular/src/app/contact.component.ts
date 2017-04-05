d15b43a621b51fbd87e425f797bd097199a3600b
Alex Gómez Uceda <alexyuncos96@gmail.com>
2017-04-05 23:35:24 +0200

    Templates view OK.
    Templates view OK.

--- WePlanningAngular/src/app/contact.component.ts
+++ WePlanningAngular/src/app/contact.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './contact.component.html'
+  templateUrl: './contact.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class ContactComponent {
 }
--- WePlanningAngular/src/app/index.component.ts
+++ WePlanningAngular/src/app/index.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-root',
-  templateUrl: './index.component.html'
+  templateUrl: './index.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class IndexComponent {
 }
