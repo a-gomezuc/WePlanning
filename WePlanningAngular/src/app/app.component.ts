b0fd8257fd2ce5546256c9a89571c1cdae400a51
Alex Gómez Uceda <alexyuncos96@gmail.com>
2017-04-06 22:56:16 +0200

    Index template similar to original.

--- WePlanningAngular/src/app/app.component.ts
+++ WePlanningAngular/src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+export class Plan{
+  
+  constructor(private id:number, 
+  private title:string, 
+  private category:string, 
+  private author:string, 
+  private place:string, 
+  private address:string,
+  private prize:number,
+  private date: string){}
+
+  
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- WePlanningAngular/src/app/index.component.ts
+++ WePlanningAngular/src/app/index.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import {Plan} from './app.component'
 @Component({
   selector: 'app-root',
   templateUrl: './index.component.html',
   styleUrls: ['./app.component.css']
 })
 export class IndexComponent {
+    private plan = new Plan(1,"Carrera","Deportes","Guillermo","Madrid","URJC Vicálvaro",12,"5-06-2017");
 }
