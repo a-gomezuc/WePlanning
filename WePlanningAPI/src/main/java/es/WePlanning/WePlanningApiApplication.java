367e36407d9cbbbca102279ece161164d49f1901
Jorge0594 <jsm0594@gmail.com>
2017-04-20 19:46:50 +0200

    New CORS Added and working

--- WePlanningAPI/src/main/java/es/WePlanning/WePlanningApiApplication.java
+++ WePlanningAPI/src/main/java/es/WePlanning/WePlanningApiApplication.java
@@ -11,16 +11,32 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WePlanningApiApplication {
 
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
-	}
+
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurerAdapter() {
+
+            @Override
+
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
+
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+
+                .allowCredentials(true).maxAge(3600);
+
+            }
+
+        };
+
+    }
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(WePlanningApiApplication.class, args);
 	}
 }
+
